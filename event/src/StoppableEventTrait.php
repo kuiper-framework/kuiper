f3e57ba91b391c29542120944617fee39a166a05
Ye Wenbin <wenbinye@gmail.com>
2020-05-10 23:47:07 +0800

    add event StoppableEventTrait

---
+++ event/src/StoppableEventTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace kuiper\event;
+
+trait StoppableEventTrait
+{
+    private $propagationStopped = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isPropagationStopped(): bool
+    {
+        return $this->propagationStopped;
+    }
+
+    /**
+     * Stops the propagation of the event to further event listeners.
+     *
+     * If multiple event listeners are connected to the same event, no
+     * further event listener will be triggered once any trigger calls
+     * stopPropagation().
+     */
+    public function stopPropagation(): void
+    {
+        $this->propagationStopped = true;
+    }
+}
--- swoole/src/event/AbstractServerEvent.php
+++ swoole/src/event/AbstractServerEvent.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace kuiper\swoole\event;
 
+use kuiper\event\StoppableEventTrait;
 use kuiper\swoole\server\ServerInterface;
 use Psr\EventDispatcher\StoppableEventInterface;
 
 abstract class AbstractServerEvent implements StoppableEventInterface
 {
-    private $propagationStopped = false;
+    use StoppableEventTrait;
 
     /**
      * @var ServerInterface
@@ -24,25 +25,5 @@ abstract class AbstractServerEvent implements StoppableEventInterface
     public function setServer(ServerInterface $server): void
     {
         $this->server = $server;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function isPropagationStopped(): bool
-    {
-        return $this->propagationStopped;
-    }
-
-    /**
-     * Stops the propagation of the event to further event listeners.
-     *
-     * If multiple event listeners are connected to the same event, no
-     * further event listener will be triggered once any trigger calls
-     * stopPropagation().
-     */
-    public function stopPropagation(): void
-    {
-        $this->propagationStopped = true;
     }
 }
