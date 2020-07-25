b68dd52b4fb7ee5049aa10fff2367057869cf5cf
Ye Wenbin <wenbinye@gmail.com>
2020-07-25 22:30:18 +0800

    add annotation cache

--- annotations/src/AnnotationReader.php
+++ annotations/src/AnnotationReader.php
@@ -1,23 +1,130 @@
 <?php
 
+declare(strict_types=1);
+
 namespace kuiper\annotations;
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\Reader;
 
-class AnnotationReader extends \Doctrine\Common\Annotations\AnnotationReader implements AnnotationReaderInterface
+class AnnotationReader implements AnnotationReaderInterface
 {
     /**
      * @var AnnotationReaderInterface
      */
     private static $INSTANCE;
 
+    /**
+     * @var Reader
+     */
+    private $delegate;
+
+    /**
+     * @var array
+     */
+    private $loadedAnnotations;
+
+    /**
+     * AnnotationReader constructor.
+     */
+    public function __construct(Reader $reader)
+    {
+        $this->delegate = $reader;
+    }
+
     public static function getInstance(): AnnotationReaderInterface
     {
         if (!self::$INSTANCE) {
             AnnotationRegistry::registerLoader('class_exists');
-            self::$INSTANCE = new self();
+            self::$INSTANCE = new self(new \Doctrine\Common\Annotations\AnnotationReader());
         }
 
         return self::$INSTANCE;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClassAnnotations(\ReflectionClass $class)
+    {
+        $cacheKey = $class->getName();
+
+        if (!isset($this->loadedAnnotations[$cacheKey])) {
+            $this->loadedAnnotations[$cacheKey] = $this->delegate->getClassAnnotations($class);
+        }
+
+        return $this->loadedAnnotations[$cacheKey];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClassAnnotation(\ReflectionClass $class, $annotationName)
+    {
+        foreach ($this->getClassAnnotations($class) as $annot) {
+            if ($annot instanceof $annotationName) {
+                return $annot;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethodAnnotations(\ReflectionMethod $method)
+    {
+        $class = $method->getDeclaringClass();
+        $cacheKey = $class->getName().'#'.$method->getName();
+
+        if (!isset($this->loadedAnnotations[$cacheKey])) {
+            $this->loadedAnnotations[$cacheKey] = $this->delegate->getMethodAnnotations($method);
+        }
+
+        return $this->loadedAnnotations[$cacheKey];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethodAnnotation(\ReflectionMethod $method, $annotationName)
+    {
+        foreach ($this->getMethodAnnotations($method) as $annot) {
+            if ($annot instanceof $annotationName) {
+                return $annot;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPropertyAnnotations(\ReflectionProperty $property)
+    {
+        $class = $property->getDeclaringClass();
+        $cacheKey = $class->getName().'$'.$property->getName();
+
+        if (!isset($this->loadedAnnotations[$cacheKey])) {
+            $this->loadedAnnotations[$cacheKey] = $this->delegate->getPropertyAnnotations($property);
+        }
+
+        return $this->loadedAnnotations[$cacheKey];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPropertyAnnotation(\ReflectionProperty $property, $annotationName)
+    {
+        foreach ($this->getPropertyAnnotations($property) as $annot) {
+            if ($annot instanceof $annotationName) {
+                return $annot;
+            }
+        }
+
+        return null;
     }
 }
--- db/src/annotation/Entity.php
+++ db/src/annotation/Entity.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace kuiper\db\annotation;
 
-use Doctrine\Common\Annotations\Annotation\Required;
-
 /**
  * @Annotation
  * @Target("CLASS")
  */
 final class Entity implements Annotation
 {
-    /**
-     * @var string
-     * @Required()
-     */
-    public $value;
 }
