78bc84b6ba1f59f1dc1881f7c9bde0ebe6870d2d
Ye Wenbin <wenbinye@gmail.com>
2020-07-26 15:59:07 +0800

    seed random in worker start

--- swoole/src/listener/WorkerStartEventListener.php
+++ swoole/src/listener/WorkerStartEventListener.php
@@ -31,6 +31,20 @@ class WorkerStartEventListener implements EventListenerInterface, LoggerAwareInt
      */
     public function __invoke($event): void
     {
+        $this->changeProcessTitle($event);
+        $this->seedRandom();
+    }
+
+    public function getSubscribedEvent(): string
+    {
+        return WorkerStartEvent::class;
+    }
+
+    /**
+     * @param WorkerStartEvent $event
+     */
+    private function changeProcessTitle($event): void
+    {
         $serverName = $event->getServer()->getServerConfig()->getServerName();
         $title = sprintf('%s: %s%s %d process', $serverName,
             ($event->getServer()->isTaskWorker() ? 'task ' : ''), ProcessType::WORKER, $event->getWorkerId());
@@ -38,8 +52,11 @@ class WorkerStartEventListener implements EventListenerInterface, LoggerAwareInt
         $this->logger->debug(static::TAG."start worker {$title}");
     }
 
-    public function getSubscribedEvent(): string
+    /**
+     * @see https://wiki.swoole.com/#/getting_started/notice?id=mt_rand%e9%9a%8f%e6%9c%ba%e6%95%b0
+     */
+    private function seedRandom(): void
     {
-        return WorkerStartEvent::class;
+        mt_srand();
     }
 }
