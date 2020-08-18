d2fdd17f2be6fd29bbb82a99c141657c36a77399
Ye Wenbin <yewenbin@winwin-inc.com>
2020-08-18 11:37:32 +0800

    添加 http log 参数

--- http-client/src/HttpClientConfiguration.php
+++ http-client/src/HttpClientConfiguration.php
@@ -6,9 +6,13 @@ namespace kuiper\http\client;
 
 use DI\Annotation\Inject;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
 use kuiper\di\annotation\Bean;
 use kuiper\di\annotation\ConditionalOnClass;
 use kuiper\di\annotation\Configuration;
+use kuiper\logger\LoggerFactoryInterface;
 use kuiper\swoole\pool\PoolFactoryInterface;
 
 /**
@@ -19,11 +23,25 @@ class HttpClientConfiguration
 {
     /**
      * @Bean()
-     * @Inject({"httpClientConfig": "application.http-client"})
+     * @Inject({"options": "application.http-client"})
      */
-    public function httpClient(HttpClientFactoryInterface $httpClientFactory, ?array $httpClientConfig): ClientInterface
+    public function httpClient(HttpClientFactoryInterface $httpClientFactory, LoggerFactoryInterface $loggerFactory, ?array $options): ClientInterface
     {
-        return $httpClientFactory->create($httpClientConfig ?? []);
+        if (!isset($options['handler'])) {
+            $options['handler'] = HandlerStack::create();
+        }
+        if (!empty($options['logging'])) {
+            $logger = $loggerFactory->create(ClientInterface::class);
+            $format = strtoupper($options['log-format'] ?? 'clf');
+            if (defined(MessageFormatter::class.'::'.$format)) {
+                $format = constant(MessageFormatter::class.'::'.$format);
+            }
+            $formatter = new MessageFormatter($format);
+            $middleware = Middleware::log($logger, $formatter, strtolower($options['log-level'] ?? 'info'));
+            $options['handler']->push($middleware);
+        }
+
+        return $httpClientFactory->create($options);
     }
 
     /**
