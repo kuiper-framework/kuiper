9e4a7037ca4fff5c4537ffaa6000ded39badece8
Ye Wenbin <yewenbin@winwin-inc.com>
2021-09-13 17:24:58 +0800

    add swoole table cache

---
+++ cache/src/SwooleTableCache.php
@@ -0,0 +1,170 @@
+<?php
+
+declare(strict_types=1);
+
+namespace kuiper\cache;
+
+use Psr\SimpleCache\CacheInterface;
+use Swoole\Table;
+
+class SwooleTableCache implements CacheInterface
+{
+    public const KEY_DATA = 'data';
+    public const KEY_EXPIRES = 'expires';
+    /**
+     * @var Table
+     */
+    private $table;
+
+    /**
+     * @var int
+     */
+    private $ttl;
+
+    /**
+     * @param int $ttl
+     * @param int $capacity number of entries to save
+     * @param int $size     size for the data
+     */
+    public function __construct(int $ttl = 60, int $capacity = 1024, int $size = 512)
+    {
+        $this->table = new Table($capacity);
+        $this->table->column(self::KEY_DATA, Table::TYPE_STRING, $size);
+        $this->table->column(self::KEY_EXPIRES, Table::TYPE_INT, 4);
+        $this->table->create();
+        $this->ttl = $ttl;
+    }
+
+    /**
+     * @return Table
+     */
+    public function getTable(): Table
+    {
+        return $this->table;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTtl(): int
+    {
+        return $this->ttl;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key, $default = null)
+    {
+        $result = $this->table->get($key);
+        if ($result && time() < $result[self::KEY_EXPIRES]) {
+            return $this->unserialize($result[self::KEY_DATA]);
+        }
+
+        return $default;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        $this->table->set($key, [
+            self::KEY_DATA => $this->serialize($value),
+            self::KEY_EXPIRES => time() + ($ttl ?? $this->ttl),
+        ]);
+
+        return true;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return string
+     */
+    protected function serialize($value): string
+    {
+        return \serialize($value);
+    }
+
+    /**
+     * @param string $data
+     *
+     * @return mixed
+     */
+    protected function unserialize(string $data)
+    {
+        return \unserialize($data, ['allowed_classes' => true]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($key)
+    {
+        $this->table->del($key);
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear()
+    {
+        $keys = [];
+        foreach ($this->table as $key => $row) {
+            $keys[] = $key;
+        }
+        $this->deleteMultiple($keys);
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        $result = [];
+        foreach ($keys as $key) {
+            $result[$key] = $this->get($key, $default);
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        foreach ($values as $key => $value) {
+            $this->set($key, $value, $ttl);
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteMultiple($keys)
+    {
+        foreach ($keys as $key) {
+            $this->table->del($key);
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($key)
+    {
+        $expire = $this->table->get($key, self::KEY_EXPIRES);
+
+        return isset($expire) && time() < $expire;
+    }
+}
--- rpc/src/servicediscovery/SwooleTableServiceEndpointCache.php
+++ rpc/src/servicediscovery/SwooleTableServiceEndpointCache.php
@@ -4,149 +4,34 @@ declare(strict_types=1);
 
 namespace kuiper\rpc\servicediscovery;
 
-use Psr\SimpleCache\CacheInterface;
-use Swoole\Table;
+use kuiper\cache\SwooleTableCache;
 
 /**
  * 存储服务地址
- *  - routes 里记录的数据为 "tcp://172.16.0.1:10204?timeout=2\ntcp://172.16.0.1:10204?timeout=2".
- *
- * Class SwooleTableRegistryCache
  */
-class SwooleTableServiceEndpointCache implements CacheInterface
+class SwooleTableServiceEndpointCache extends SwooleTableCache
 {
     public const KEY_DATA = 'data';
     public const KEY_EXPIRES = 'expires';
-    /**
-     * @var Table
-     */
-    private $table;
 
-    /**
-     * @var int
-     */
-    private $ttl;
-
-    /**
-     * SwooleTableRegistryCache constructor.
-     *
-     * @param int $ttl
-     * @param int $capacity number of address to save
-     * @param int $size     size for the address 每个服务长度大约50个字符，默认2046长度可以存储最多40台服务器的地址
-     */
     public function __construct(int $ttl = 60, int $capacity = 256, int $size = 2046)
     {
-        $this->table = new Table($capacity);
-        $this->table->column(self::KEY_DATA, Table::TYPE_STRING, $size);
-        $this->table->column(self::KEY_EXPIRES, Table::TYPE_INT, 4);
-        $this->table->create();
-        $this->ttl = $ttl;
+        parent::__construct($ttl, $capacity, $size);
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function get($key, $default = null)
+    protected function serialize($value): string
     {
-        $result = $this->table->get($key);
-        if ($result && time() < $result[self::KEY_EXPIRES]) {
-            return $this->decode($result[self::KEY_DATA]);
-        }
-
-        return $default;
+        return (string) $value;
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function set($key, $value, $ttl = null)
-    {
-        $this->table->set($key, [
-            self::KEY_DATA => $this->encode($value),
-            self::KEY_EXPIRES => time() + ($ttl ?? $this->ttl),
-        ]);
-
-        return true;
-    }
-
-    private function encode(ServiceEndpoint $endpoint): string
-    {
-        return (string) $endpoint;
-    }
-
-    private function decode(string $data): ServiceEndpoint
+    protected function unserialize(string $data)
     {
         return ServiceEndpoint::fromString($data);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function delete($key)
-    {
-        $this->table->del($key);
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function clear()
-    {
-        $keys = [];
-        foreach ($this->table as $key => $row) {
-            $keys[] = $key;
-        }
-        $this->deleteMultiple($keys);
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getMultiple($keys, $default = null)
-    {
-        $result = [];
-        foreach ($keys as $key) {
-            $result[$key] = $this->get($key, $default);
-        }
-
-        return $result;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setMultiple($values, $ttl = null)
-    {
-        foreach ($values as $key => $value) {
-            $this->set($key, $value, $ttl);
-        }
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function deleteMultiple($keys)
-    {
-        foreach ($keys as $key) {
-            $this->table->del($key);
-        }
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function has($key)
-    {
-        $expire = $this->table->get($key, self::KEY_EXPIRES);
-
-        return isset($expire) && time() < $expire;
     }
 }
