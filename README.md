f5e6618a63b103cc7f578cf0708c119a9974d85d
Ye Wenbin <yewenbin@winwin-inc.com>
2021-09-23 11:57:35 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kuiper PHP Framework
+
+Kuiper 希望能像 Spring Boot 一样简化 PHP 应用开发，通过简单的配置甚至没有配置就能运行基于 Swoole 的 PHP 服务。
+
+## 文档
+
+https://kuiper.wiki
---
+++ bin/release.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+NOW=$(date +%s)
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+VERSION=$(git tag --points-at HEAD)
+BASEPATH=$(cd `dirname $0`; cd ../src/; pwd)
+
+# Always prepend with "v"
+if [[ $VERSION != v*  ]]
+then
+    VERSION="v$VERSION"
+fi
+
+repos="annotations cache db di event helper http-client jsonrpc logger reflection resilience rpc rpc-registry serializer swoole tars web"
+
+for REMOTE in $repos
+do
+    echo ""
+    echo ""
+    echo "Cloning $REMOTE";
+    TMP_DIR="/tmp/kuiper-split"
+    REMOTE_URL="git@github.com:kuiper-framework/$REMOTE.git"
+
+    rm -rf $TMP_DIR;
+    mkdir $TMP_DIR;
+
+    (
+        cd $TMP_DIR;
+
+        git clone $REMOTE_URL .
+        git checkout "$CURRENT_BRANCH";
+
+        if [[ $(git log --pretty="%d" -n 1 | grep tag --count) -eq 0 ]]; then
+            echo "Releasing $REMOTE"
+            git tag $VERSION
+            git push origin --tags
+        fi
+    )
+done
+
+TIME=$(echo "$(date +%s) - $NOW" | bc)
+
+printf "Execution time: %f seconds" $TIME
